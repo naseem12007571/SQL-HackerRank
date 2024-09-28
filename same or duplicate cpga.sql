b35db307709eb3b4eb4b2c0c9c54b38a5cfc58a1
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-09-28 23:47:54 +0530

    Create same or duplicate cpga.sql

---
+++ same or duplicate cpga.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT s1.cgpa 
+FROM student1 s1, student1 s2 
+WHERE s1.cgpa = s2.cgpa 
+AND s1.student_id != s2.student_id;
+
+
+-- geek for geek ki query h ye https://www.geeksforgeeks.org/sql-query-interview-questions/
+query no 24
